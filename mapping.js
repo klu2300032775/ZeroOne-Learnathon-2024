b98c6adb4548073f2936ba30a92db096994cc19e
klu2300032775 <163757390+klu2300032775@users.noreply.github.com>
2024-10-18 17:42:08 +0530

    2300032775_Saandeep

---
+++ mapping.js
@@ -0,0 +1,15 @@
+sum = (a, b) => {
+    return(a+b)
+  }
+sub = (a, b) => {
+ return(a-b)
+}
+div = (a, b) => {
+    return(a/b)
+  }
+mul = (a, b) => {
+    return(a*b)
+  }
+var a=30;
+var b=10;
+console.log(sum(a,b),sub(a,b),div(a,b),mul(a,b));
